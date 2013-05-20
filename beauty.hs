041ea20d5549863b7ef4777090b31f2d7bb1f775
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-05-20 16:42:31 -0400

    works

--- beauty.hs
+++ beauty.hs
@@ -1,5 +1,6 @@
 #!/usr/bin/env runhaskell
 
+import Data.Bits (popCount)
 import Control.Monad (replicateM_)
 
 data Player
@@ -10,8 +11,29 @@ instance Show Player where
   show FirstPlayer = "First Player"
   show SecondPlayer = "Second Player"
 
-whoWouldWin :: Int -> Player
-whoWouldWin n = FirstPlayer
+switchTurn :: Player -> Player
+switchTurn FirstPlayer = SecondPlayer
+switchTurn SecondPlayer = FirstPlayer
+
+lg = floor . logBase 2 . fromIntegral
+
+-- Number of 1's in binary representation
+beauty = popCount
+
+asBeautifulAs :: Int -> [Int]
+asBeautifulAs n = [ k | k <- [0 .. lg n], beauty (n - (2^k)) == beauty n ]
+
+-- Determines which player would win a game,
+-- Given n and a starting player
+whoWouldWin :: Player -> Int -> Player
+whoWouldWin p n
+  | n < 1 = p --switchTurn p
+  | n == 1 = switchTurn p
+  | length ks >= 1 = whoWouldWin (switchTurn p) n'
+  | otherwise = switchTurn p
+  where
+    ks = asBeautifulAs n
+    n' = n - (2^(head ks))
 
 main :: IO ()
 main = do
@@ -21,4 +43,4 @@ main = do
   replicateM_ testCases $ do
     n' <- getLine
     let n = read n' :: Int
-    (print . whoWouldWin) n
+    (print . whoWouldWin FirstPlayer) n
